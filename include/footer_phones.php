2b54ee481be79b02e8f5d72239fc3c3a96ba2125
Vadim Sedelkin <v.sedelkin@quarta-hunt.ru>
2025-03-25 16:20:28 +0300

    fix: замена номеров

--- include/footer_phones.php
+++ include/footer_phones.php
@@ -1,9 +1,9 @@
 <div class="footer__address">
 	<div class="footer__address-item">
- <a class="ui-link" href="tel:8804441476">8 (800) 444-14-76</a>
+ <a class="ui-link" href="tel:+78004441476">8 (800) 444-14-76</a>
 	</div>
 	<div class="footer__address-item">
- <a class="ui-link" href="tel:+7 8126048060">+7 (812) 604-80-60 </a>
+ <a class="ui-link" href="tel:+78123366099">+7 (812) 336-60-99 </a>
 	</div>
 	<div class="footer__address-item">
  <a class="ui-link" href="mailto:shop@shottime.ru">shop@shottime.ru</a>
