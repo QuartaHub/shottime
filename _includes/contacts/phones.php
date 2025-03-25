412a46d52b42054321fc54cb7fac193083965115
Vadim Sedelkin <v.sedelkin@quarta-hunt.ru>
2025-03-25 16:22:04 +0300

    fix: замена номеров

--- _includes/contacts/phones.php
+++ _includes/contacts/phones.php
@@ -1,2 +1,2 @@
-<a href="tel:8804441476" class="contacts-info__text">8 800 444-14-76</a>
+<a href="tel:+78004441476" class="contacts-info__text">8 800 444-14-76</a>
 <a href="tel:+78123366099" class="contacts-info__text">+7 (812) 336-60-99</a>
